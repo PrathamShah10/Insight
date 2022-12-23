1aaf011075dc07cc6f13e41c7e87a36af9b08649
Pratham Shah <prathamsanjayshah2001@gmail.com>
2022-12-23 10:17:32 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
   return (
     <div className="app">
       <div className="app_nav">
-        <h1>Insite</h1>
+        <h1>Insight</h1>
       </div>
       <div className="app_boards_container">
         <div className="app_boards">
